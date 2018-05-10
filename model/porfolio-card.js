2d53690f939b81f12dd8396e483f14a3717cc2c6
Jared Gilpin <irishkungfu@localhost.localdomain>
2018-05-09 23:46:29 -0500

    redesign porfolio section, created basic model

---
+++ model/porfolio-card.js
@@ -0,0 +1,67 @@
+var portfolioCards = [
+  {
+    name: "Quiz Break",
+    tags: ["API", "jQuery"],
+    tagline: "Test your wit in this addictive game!",
+    logo: "./assets/images/icons/quizbreak-logo.png",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://irishkungfu.github.io/quizbreak/"
+  },
+  {
+    name: "Eat Da Burger",
+    tags: ["Express", "Node.js", "ORM"],
+    tagline: "Demo app using ORMs.",
+    logo: "./assets/images/icons/burger-logo.png",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://whispering-cove-98673.herokuapp.com/"
+  },
+  {
+    name: "Pepper & Carrot",
+    tags: ["Javascript", "jQuery", "CSS", "HTML"],
+    tagline: "Join the wizarding fray and test your skills!",
+    logo: "./assets/images/icons/pepper-and-carrot-logo.png",
+    logoSetting: "10%",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://irishkungfu.github.io/pepper-and-carrot/"
+  },
+  {
+    name: "Hangman",
+    tags: ["Javascript", "jQuery", "CSS", "HTML"],
+    tagline:
+      "Play classic movie hangman. Guess the titles and see a clip from your favorite classics.",
+    logo: "./assets/images/icons/hangman.png",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://irishkungfu.github.io/Hangman-Game/"
+  },
+  {
+    name: "Gif Search",
+    tags: ["Javascript", "jQuery", "CSS", "HTML"],
+    tagline:
+      "Search for your favorite gifs. But no debating how to pronounce gif.",
+    logo: "./assets/images/icons/gif-search-logo.png",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://irishkungfu.github.io/giphy-API/"
+  },
+  {
+    name: "Train Schedule",
+    tags: ["Javascript", "jQuery", "CSS", "HTML"],
+    tagline:
+      "Determine when the next train is coming and update the train schedule.",
+    logo: "./assets/images/icons/train-icon.png",
+    screenshot: "url goes here",
+    description: "Description goes here (detailed)",
+    active: true,
+    url: "https://irishkungfu.github.io/Train-Schedule/"
+  }
+];
+module.exports = portfolioCards;
